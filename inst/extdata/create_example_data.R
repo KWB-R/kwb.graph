97ef54ccb6347dc97b9977ac345baff21143074a
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-07-14 23:28:47 +0200

    Provide example network as CSV file

---
+++ inst/extdata/create_example_data.R
@@ -0,0 +1,19 @@
+load("~/github-repos/kwb.graph/data/kurasNetwork.RData")
+x0 <- stats::setNames(kurasNetwork, c("upstream", "downstream"))
+node_names <- unique(c(x0$upstream, x0$downstream))
+node_ids <- kwb.utils::createIdAlong(node_names, base_name = "node")
+node_ids <- gsub("^node_", "n", node_ids)
+
+x <- x0
+x$node_us <- node_ids[match(x$upstream, node_names)]
+x$node_ds <- node_ids[match(x$downstream, node_names)]
+
+exampleData <- x[, -(1:2)]
+head(exampleData)
+
+write.csv(
+  exampleData, 
+  file = "~/github-repos/kwb.graph/inst/extdata/exampleNetwork.csv",
+  row.names = FALSE,
+  quote = FALSE
+)
