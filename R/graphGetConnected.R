5931c8e76ea8e59eba6468629239e1abd2acecd2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-07-18 01:48:52 +0200

    Clean and improve graphGetConnected.R
    - use kwb.utils::select[Columns|Elements]()
    - use simpler (shorter) names

--- R/graphGetConnected.R
+++ R/graphGetConnected.R
@@ -2,14 +2,14 @@
 
 #' Get Connected Links
 #'
-#' @param x data frame with each row representing a link of the network. Required
-#' columns: \emph{us_node_id}, \emph{ds_node_id} 
+#' @param x data frame with each row representing a link of the network.
+#'   Required columns: \emph{us_node_id}, \emph{ds_node_id}
 #' @param upstream if TRUE (upstream), if FALSE (downstream), default: TRUE
 #' @param version 1: R-implementation, 2: C-implementation, (default: 1)
 #' @param dbg default: FALSE
-#' @param ... additional arguments passed to  \code{\link{getConnectedLinks.C}}
-#'
+#' @param ... additional arguments passed to \code{\link{getConnectedLinks.C}}
 #' @return get connected links
+#' @importFrom kwb.utils catAndRun
 #' @export
 #' @useDynLib kwb.graph C_getDirectLinks C_getConnectedLinks
 #' @examples 
@@ -48,27 +48,34 @@
 #' )
 #' 
 #' boxplot(runtime ~ version, data = runtimeData)
-#' 
 getConnectedLinks <- function(x, upstream = TRUE, version = 1, dbg = FALSE, ...)
 {
   n <- nrow(x)
   
   # get the row indices of all upstream (or downstream) connected links
   if (version == 1) {
-    direct.us <- getDirectLinks.R(x = x, upstream = upstream)
-    connectedLinks <- getConnectedLinks.R(direct.us, dbg = dbg)
-  }
-  else {
-    if (dbg) cat("getting direct links... ")
-    direct.us <- getDirectLinks.C(x = x)
-    if (dbg) cat("ok.\ngetting connected links...\n")
-    connectedLinks <- getConnectedLinks.C(direct.us, version = version-1, dbg = dbg, ...)
-    if (dbg) cat("getting connected links ok.\n")
+    
+    direct <- getDirectLinks.R(x = x, upstream = upstream)
+    connected <- getConnectedLinks.R(direct, dbg = dbg)
+    
+  } else {
+    
+    direct <- kwb.utils::catAndRun(
+      dbg = dbg, 
+      "Getting direct links",
+      getDirectLinks.C(x = x)
+    )
+    
+    connected <- kwb.utils::catAndRun(
+      dbg = dbg, 
+      "Getting connected links",
+      getConnectedLinks.C(direct, version = version - 1, dbg = dbg, ...)
+    )
   }
   #else {
   #  stop("version must be 1 (R-implementation) or 2 (C-implementation)!")
   #}
-  connectedLinks
+  connected
 }
 
 # getDirectLinks.R -------------------------------------------------------------
@@ -78,137 +85,124 @@ getConnectedLinks <- function(x, upstream = TRUE, version = 1, dbg = FALSE, ...)
 #' @param x data frame with each row representing a link of the network. Required
 #' columns: \emph{us_node_id}, \emph{ds_node_id} 
 #' @param upstream if TRUE (upstream), if FALSE (downstream), default: TRUE
-#'
-#' @return get direct links with R implementation
+#' @return list with as many elements as there are rows in \code{x}
 #' @export
-
-getDirectLinks.R <- function
-(
-  x,
-  upstream = TRUE
-) 
+getDirectLinks.R <- function(x, upstream = TRUE) 
 {  
-  # initialise boolean matrix where each row represents a link and the columns
-  # holding TRUE represent the directly connected upstream links
-  linked <- list()
+  # For each link (each row in x), find the row indices of directly (upstream
+  # or downstream) connected links
+  us <- "us_node_id"
+  ds <- "ds_node_id"
   
-  # set direct.us to TRUE at crossings between connected links
-  for (i in seq_len(nrow(x))) {
-    if (upstream) {
-      linked[[i]] <- which(x[i, "us_node_id"] == x[, "ds_node_id"])
-    }
-    else {
-      linked[[i]] <- which(x[i, "ds_node_id"] == x[, "us_node_id"])
-    }
-  }
-  
-  linked
+  lapply(seq_len(nrow(x)), function(i) which(`==`(
+    x[i, ifelse(upstream, us, ds)],
+    x[ , ifelse(upstream, ds, us)]
+  )))
 }
 
 # getDirectLinks.C -------------------------------------------------------------
+
 #' Get Direct Links (C Implementation)
 #'
-#' @param x data frame with each row representing a link of the network. Required
-#' columns: \emph{us_node_id}, \emph{ds_node_id} 
+#' @param x data frame with each row representing a link of the network.
+#'   Required columns: \emph{us_node_id}, \emph{ds_node_id}
 #' @param MAX_DIRECT_CONNECTIONS default: 5
 #' @param dbg default: FALSE
-#'
 #' @return get direct links with C implementation
+#' @importFrom kwb.utils selectColumns selectElements
 #' @export
-#'
-
-getDirectLinks.C <- function
-(
-  x, MAX_DIRECT_CONNECTIONS = 5, dbg = FALSE
-)
+getDirectLinks.C <- function(x, MAX_DIRECT_CONNECTIONS = 5, dbg = FALSE)
 {
+  getcol <- kwb.utils::selectColumns
+  getele <- kwb.utils::selectElements
+  
   n <- nrow(x)  
   
-  numberOfFound <- rep(as.integer(0), n)
-  indicesOfFound <- as.integer(rep(0, MAX_DIRECT_CONNECTIONS*n))
+  n_found <- rep(as.integer(0), n)
+  i_found <- as.integer(rep(0, MAX_DIRECT_CONNECTIONS * n))
   
   result <- .C(
     "C_getDirectLinks", 
     length = as.integer(n), 
-    upstreamNodes = x$us_node_id,
-    downstreamNodes = x$ds_node_id,
-    numberOfFound = numberOfFound,
-    indicesOfFound = indicesOfFound,
+    upstreamNodes = getcol(x, "us_node_id"),
+    downstreamNodes = getcol(x, "ds_node_id"),
+    numberOfFound = n_found,
+    indicesOfFound = i_found,
     maxNumber = as.integer(MAX_DIRECT_CONNECTIONS),
     dbg = as.integer(dbg)
     #,    PACKAGE = "kwb.graph"    
   )
   
-  if (max(result$numberOfFound) > MAX_DIRECT_CONNECTIONS) {
+  n_found <- getele(result, "numberOfFound")
+  i_found <- getele(result, "indicesOfFound") 
+  
+  if (max(n_found) > MAX_DIRECT_CONNECTIONS) {
     stop("It seems that you need to increase MAX_DIRECT_CONNECTIONS!")
   }
   
-  indexMatrix <- matrix(result$indicesOfFound, ncol = MAX_DIRECT_CONNECTIONS)
+  M <- matrix(i_found, ncol = MAX_DIRECT_CONNECTIONS)
   
-  lapply(seq_len(n), function(i) {
-    indexMatrix[i, seq_len(result$numberOfFound[i])]
-  })
+  lapply(seq_len(n), function(i) M[i, seq_len(n_found[i])])
 }
 
 # getConnectedLinks.R ----------------------------------------------------------
+
 #' Get Connected Links (R Implementation)
 #'
 #' @param directly.connected directly.connected
 #' @param dbg default: FALSE
-#'
 #' @return get connected links with R implementation
 #' @export
 #' @importFrom kwb.utils catIf
-getConnectedLinks.R <- function
-(
-  directly.connected, 
-  dbg = FALSE
-)
+getConnectedLinks.R <- function(directly.connected, dbg = FALSE)
 {
-  # initialise result list
+  # Initialise result list
   all.connected <- list()
   
   n <- length(directly.connected)
   
   blocksize <- max(1, n %/% 80)
   
-  # loop through list of direct links
+  # Loop through list of direct links
   for (i in seq_len(n)) {
     
     kwb.utils::catIf(dbg && i %% blocksize == 0, ".")
     
-    # initialise vector of indices of connected links
+    # Initialise vector of indices of connected links
     all.connected[[i]] <- integer(0)
     
-    # initialise boolean vector holding TRUE at indices representing the links
+    # Initialise boolean vector holding TRUE at indices representing the links
     # that are connected directly or indirectly to the current link i
     is.connected <- logical(length = n)
     
-    # start queue of indices of connected links with indices of directly
+    # Start queue of indices of connected links with indices of directly
     # connected links
     queue <- directly.connected[[i]] 
     
     while (length(queue) > 0) {
       
-      # get and remove next index from queue
-      link.index <- queue[1]
-      queue <- queue[-1]        
+      # Get and remove next index from queue
+      link.index <- queue[1L]
+      queue <- queue[-1L]
       
       is.connected[link.index] <- TRUE
       
       if (link.index < i) {
-        # if link with index link.index was already considered (before the
+        
+        # If link with index link.index was already considered (before the
         # current link i) save full information on links that are connected to
         # the link with index link.index in the logical vector is.connected.
         is.connected[all.connected[[link.index]]] <- TRUE
-      } 
-      else {
-        # add indices of directly connected links to the queue
+        
+      } else {
+        
+        # Add indices of directly connected links to the queue
         queue <- c(queue, directly.connected[[link.index]])
       }
-    } # of while
+      
+    } # End of while
     
-    # save indices of connected links
+    # Save indices of connected links
     all.connected[[i]] <- which(is.connected)
   }
   
@@ -218,66 +212,54 @@ getConnectedLinks.R <- function
 }
 
 # getConnectedLinks.C ----------------------------------------------------------
-#' #' Get Connected Links (C Implementation)
+
+#' Get Connected Links (C Implementation)
 #'
 #' @param directLinks directLinks 
 #' @param resultSize default: 60000
 #' @param queueSize default: 1024*1024
 #' @param version version of C implementation: 1,2 or 3 (default: 1)
 #' @param dbg default: FALSE
-#'
-#' @return get connected links with C implementation
+#' @return data.frame with columns \code{origin}, \code{linked} and attribute
+#'   \code{numberOfConnected}
+#' @importFrom kwb.utils catIf
 #' @export
-#'
-
-getConnectedLinks.C <- function # getConnectedLinks.C
-### getConnectedLinks.C
-(
+getConnectedLinks.C <- function(
   directLinks, resultSize = 60000, queueSize = 1024*1024, version = 1,
   dbg = FALSE
 )
 {
-  # Prepare the input data
   n <- length(directLinks)
   
-  # vector containing the number of directly connected links for each link
-  numberOfLinks <- sapply(directLinks, length)
+  # Number of directly connected links for each link
+  n_links <- lengths(directLinks)
   
-  # matrix with each row representing a link. Column i contains the ID of the 
+  # Matrix with each row representing a link. Column i contains the ID of the 
   # i-th directly connected link. If there is no such link the matrix element is 
-  # - 1.
-  maxDirectLinks = max(numberOfLinks)
+  # -1.
+  i_direct <- matrix(-1L, nrow = n, ncol = max(n_links))
   
-  indicesOfDirectLinks <- matrix(
-    rep(as.integer(-1), n * maxDirectLinks), ncol = maxDirectLinks
-  )
-  
-  FUN <- function(i) {
-    n.links <- numberOfLinks[[i]]
-    onerow <- indicesOfDirectLinks[i, ]
-    if (n.links > 0) {
+  links.matrix <- t(sapply(seq_len(n), FUN = function(i) {
+    n.links <- n_links[[i]]
+    onerow <- i_direct[i, ]
+    if (n.links > 0L) {
       onerow[seq_len(n.links)] <- as.integer(directLinks[[i]])
     }
     onerow
-  }
+  }))    
   
-  links.matrix <- t(sapply(seq_len(n), FUN = FUN))    
+  kwb.utils::catIf(dbg, "\ngetConnectedLinks.C:\n", sprintf(
+    "Calling C_getConnectedLinks(%d, queueSize = %d, dbg = %d, version = %d)\n", 
+    n, queueSize, dbg, version
+  ))
   
-  # in C, a one-dimensional array is used, created by putting one column after 
-  # the other
-  links.vector <- as.vector(links.matrix)
-
-  if (dbg) {
-    cat("\ngetConnectedLinks.C:\n",
-        sprintf("Calling C_getConnectedLinks(%d, queueSize = %d, dbg = %d, version = %d)\n", 
-                n, queueSize, dbg, version))    
-  }
-  
-  result <- .C(
+  fetch <- kwb.utils::createAccessor(.C(
     "C_getConnectedLinks", 
     in_n = as.integer(n), 
-    in_numberOfDirectLinks = as.integer(numberOfLinks),
-    in_indicesOfDirectLinks = as.integer(links.vector),
+    in_numberOfDirectLinks = as.integer(n_links),
+    # In C, a one-dimensional array is used, created by putting the columns one
+    # after another
+    in_indicesOfDirectLinks = c(links.matrix),
     out_numberOfTotalLinks = as.integer(rep(0, n)),
     inout_result_size = as.integer(resultSize),
     out_result_origin = as.integer(rep(0, resultSize)),
@@ -285,18 +267,18 @@ getConnectedLinks.C <- function # getConnectedLinks.C
     in_queueSize = as.integer(queueSize),
     in_dbg = as.integer(dbg),
     in_version = as.integer(version)
-    #,    PACKAGE = "kwb.graph"
-  )
+    #, PACKAGE = "kwb.graph"
+  ))
 
-  if (dbg) {
-    cat("\nBack in R from C_getConnectedLinks.\n")
-  }
+  kwb.utils::catIf(dbg, "\nBack in R from C_getConnectedLinks.\n")
+
+  valid <- seq_len(fetch("inout_result_size"))
   
-  valid <- seq_len(result$inout_result_size)
-  
-  linked <- data.frame(
-    origin = result$out_result_origin[valid],
-    linked = result$out_result_linked[valid])
-  
-  structure(linked, numberOfConnected = result$out_numberOfTotalLinks)
+  structure(
+    data.frame(
+      origin = fetch("out_result_origin")[valid],
+      linked = fetch("out_result_linked")[valid]
+    ), 
+    numberOfConnected = fetch("out_numberOfTotalLinks")
+  )
 }
